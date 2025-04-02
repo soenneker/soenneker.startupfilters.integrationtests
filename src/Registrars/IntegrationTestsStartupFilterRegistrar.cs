66bd321b26d5ce5c35ade61fd8d0f2f4ca7a6f81
Jake Soenneker <jake@soenneker.com>
2025-04-01 19:22:52 -0500

    Adds registrar

---
+++ src/Registrars/IntegrationTestsStartupFilterRegistrar.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Soenneker.StartupFilters.IntegrationTests.Registrars;
+
+/// <summary>
+/// An abstract xUnit test class for end-to-end integration (involving WebApplicationFactory) tests
+/// </summary>
+public static class IntegrationTestRegistrar
+{
+    /// <summary>
+    /// Adds <see cref="IntegrationTestsStartupFilter"/> as a singleton service. <para/>
+    /// </summary>
+    public static IServiceCollection AddIntegrationTestAsSingleton(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IStartupFilter, IntegrationTestsStartupFilter>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds <see cref="IntegrationTestsStartupFilter"/> as a scoped service. <para/>
+    /// </summary>
+    public static IServiceCollection AddIntegrationTestAsScoped(this IServiceCollection services)
+    {
+        services.TryAddScoped<IStartupFilter, IntegrationTestsStartupFilter>();
+
+        return services;
+    }
+}
